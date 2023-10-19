89f69dcc90713cc889637121a5d9a9349f17b20a
Mike <32255755+MikeTsak@users.noreply.github.com>
2023-10-19 14:35:41 +0300

    Added news photo editor and more copyrigths

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
+          <!-- 1. Personal Articles Section -->
     <div class="container">
       <a href="https://www.gameworld.gr/jreviews/my-listings?user=54184" class="articles-button">
         <img src="https://www.gameworld.gr/images/avatar/5763925d123ca66c2e226227.jpg" alt="Avatar" class="button-avatar" />
         Read articles by me
       </a>
 
+      <hr> <!-- Horizontal line to separate sections -->
+
+            <!-- 2. Game Rating Section -->
       <input class="input-style" type="text" v-model="prefix" placeholder="Enter game name for images" />
       <div class="rating-section">
         <div v-for="category in categories" :key="category.name" class="slider-container">
@@ -26,15 +30,51 @@
         Metacritic Score
       </button>
 
+      <hr> <!-- Horizontal line to separate sections -->
+
+          <!-- 3. Screenshot Tool -->
       <h3>Screenshot tool for reviews</h3>
       <input class="input-style" type="file" multiple @change="handleFiles" />
       (use Ctrl + Click to select multiple files)
       
       <button class="button-style" @click="processImages">Resize and Add Watermark</button>
 
+      <hr> <!-- Horizontal line to separate sections -->
+
+           <!-- 4. News Photo Editor -->     
+      <h3>News Photo Editor</h3>
+      <input type="file" class="input-style" @change="onImageChange" ref="fileInput">
+      <div class="cropper-container">
+      <div v-if="showCropper">
+
+        <vue-cropper 
+          ref="cropper"
+          :src="imgSrc"
+          :aspect-ratio="16/9"
+          :view-mode="1"
+          :guides="true">
+        </vue-cropper>
+        <div class="button-crop">
+        <button  class="button-style" @click="cropImage">Crop</button>
+        <button  class="button-style" @click="cancelCrop">Cancel</button>
+        </div>
+      </div>
+      </div>
+      
+      <img v-if="croppedImage" :src="croppedImage" alt="Cropped Image">
+      <a v-if="croppedImage" :href="croppedImage" download="cropped-image.jpg" class="download-button">Download Cropped Image</a>
+
       <footer>
         <a href="https://www.miketsak.gr" class="footer-link">
           Made by miketsak.gr
+        </a>
+        <br />
+        <a href="https://www.gameworld.gr/" class="footer-link">
+          The GameWorld logo is a © of gameworld.gr 
+        </a>
+        ||
+        <a href="https://www.metacritic.com/" class="footer-link">
+          The Metacritic logo is a © of metacritic.com
         </a>
       </footer>
 
@@ -43,11 +83,19 @@
 </template>
 
 <script>
+import VueCropper from 'vue-cropperjs';
+import 'cropperjs/dist/cropper.css';
+
 export default {
   data() {
     return {
       files: [],
       prefix: "",
+      newsImage: null,
+      imgSrc: null,
+      showCropper: false,
+      croppedImage: null,
+    showCropPopup: false,
       categories: [
         { label: 'Γραφικά', name: 'graphics', value: 5 },
         { label: 'Ήχος', name: 'sound', value: 5 },
@@ -62,6 +110,9 @@ export default {
       handler: "sanitizeGameName",
       immediate: true
     }
+  },
+  components: {
+    VueCropper
   },
   computed: {
     average() {
@@ -138,6 +189,36 @@ export default {
       const searchUrl = baseUrl + encodeURIComponent(this.prefix) + '/';
       window.open(searchUrl, '_blank');
     },
+    onImageChange(e) {
+      const file = e.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          const img = new Image();
+          img.onload = () => {
+            if (img.width !== 1024 || img.height !== 576) {
+              this.imgSrc = event.target.result;
+              this.showCropper = true;
+            } else {
+              this.croppedImage = event.target.result;
+            }
+          };
+          img.src = event.target.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    },
+    cropImage() {
+      const canvas = this.$refs.cropper.getCroppedCanvas({ width: 1024, height: 576 });
+      this.croppedImage = canvas.toDataURL();
+      this.showCropper = false;
+      this.$refs.fileInput.value = null; // Clear the file input
+    },
+    cancelCrop() {
+      this.imgSrc = null;
+      this.showCropper = false;
+      this.$refs.fileInput.value = null; // Clear the file input
+    }
   },
 };
 </script>
@@ -176,7 +257,13 @@ export default {
   align-items: center;
   margin: 10px 0;
 }
-
+hr {
+  border: 0;
+  height: 1px;
+  width: 25%;
+  background-color: #274c99; /* Light gray color */
+  margin: 10px 0; /* Add space above and below the line */
+}
 .slider-container input[type="range"] {
   flex: 1;
   margin: 0 10px;
@@ -233,7 +320,7 @@ export default {
 }
 
 .button-style:hover {
-  background-color: darkblue;
+  background-color: #274c99;
 }
 .articles-button {
   display: flex;
@@ -249,6 +336,17 @@ export default {
 
 .articles-button:hover {
   background-color: #274c99;  /* Slightly darker shade for hover effect */
+}
+
+
+.button-crop {
+  display: flex;
+  justify-content: center;
+  margin: 10px 0;
+}
+
+.button-style + .button-style {
+  margin-left: 10px;
 }
 
 .button-avatar {
@@ -270,10 +368,27 @@ footer {
   color: #3163bb;
   text-decoration: none;
   transition: color 0.3s ease;
+  position: relative;
+  display: inline-block; /* Needed to position the pseudo-element correctly */
+}
+
+.footer-link::before {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 0; /* Initial width */
+  height: 2px; /* Thickness of underline */
+  background: #274c99; /* Color of underline */
+  transition: width 0.3s ease; /* Transition for the underline */
 }
 
 .footer-link:hover {
-  color: #274c99;  /* Darkens the link color on hover for a subtle effect */
+  color: #274c99;
+}
+
+.footer-link:hover::before {
+  width: 100%; /* Full width on hover */
 }
 .metacritic-button {
   background: #3163bb;
@@ -299,4 +414,12 @@ footer {
   margin-right: 10px;
 }
 
+.cropper-container {
+  max-width: 50%; 
+  margin: 0 auto; 
+  overflow: hidden; 
+}
+.cropper-container img {
+  max-width: 100%; 
+}
 </style>
